a32744e4e3b702b74c65293c26be7bacfe1507b2
Aryan Singh <151187488+Iron-sngh@users.noreply.github.com>
2025-06-27 14:29:40 +0530

    Create quickStart.py

---
+++ quickStart.py
@@ -0,0 +1,70 @@
+"""
+this file funtions in sending emails using Gmail using Google API's
+"""
+
+from __future__ import print_function
+
+import os.path
+
+import base64
+from email.message import EmailMessage
+
+import google.auth
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+import mimetypes
+from email.mime.audio import MIMEAudio
+from email.mime.base import MIMEBase
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://mail.google.com/']
+
+    
+def gmail_send_message(to,content,subject):
+    """Create and send an email message
+    Print the returned  message id
+    Returns: Message object, including message id
+
+    Load pre-authorized user credentials from the environment.
+    TODO(developer) - See https://developers.google.com/identity
+    for guides on implementing OAuth2 for the application.
+    """
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+
+    try:
+        service = build('gmail', 'v1', credentials=creds)
+        message = EmailMessage()
+
+        message['To'] = to
+        message['From'] = 'abc@gmail.com'
+        message['Subject'] = subject
+        message.set_content(
+            content
+        )
+
+        # encoded message
+        encoded_message = base64.urlsafe_b64encode(message.as_bytes()) \
+            .decode()
+
+        create_message = {
+            'raw': encoded_message
+        }
+        # pylint: disable=E1101
+        send_message = (service.users().messages().send
+                        (userId="me", body=create_message).execute())
+        print(F'Message Id: {send_message["id"]}')
+    except HttpError as error:
+        print(F'An error occurred: {error}')
+        send_message = None
+    return send_message
+
+
+if __name__ == '__main__':
+    gmail_send_message()
